7c73c5b94712adcdf1c4daecd593ae723351b969
abdulrahman305 <owner@sauditech.link>
2025-08-17 12:21:09 +0000

    🎨 Auto-fix lint errors
    Applied: npx, isort
    
    Automatically fixed by GitOps PR automation

--- packages/cli/test/dev/fixtures/python-flask/api/user.py
+++ packages/cli/test/dev/fixtures/python-flask/api/user.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def user(path):
-    name = request.args.get('name')
-    return Response("Hello %s" % (name), mimetype='text/html')
+    name = request.args.get("name")
+    return Response("Hello %s" % (name), mimetype="text/html")
